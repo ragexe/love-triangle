b83cf5c7ad949fc68cb7e45cecafc760d6707ed6
ragexe <ragexe@mail.ru>
2018-02-20 01:48:20 +0300

    love-triangle/final

--- src/index.js
+++ src/index.js
@@ -3,5 +3,40 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    let result = 0;
+    if (preferences.length < 3){
+        return result
+    }
+
+    for (let i = 0; i < preferences.length; i++){
+        if (!preferences[i]) continue;
+        if (isPartOfTriangle(i)) {
+            result++;
+            clearLoveTriangle(i)
+        }
+    }
+
+    function clearLoveTriangle(i) {
+        preferences[preferences[preferences[i] - 1] - 1] = null;
+        preferences[preferences[i] - 1] = null;
+        preferences[i] = null;
+    }
+
+    function isPartOfTriangle(i){
+        return (
+            isInTriangle(i) &&
+            isInTriangle(preferences[i] - 1) &&
+            isInTriangle(preferences[preferences[i] - 1] - 1)
+        )
+    }
+
+    function isInTriangle(i){
+        return (
+            (i + 1) !== preferences[i] && (i + 1 === preferences[preferences[preferences[i] - 1] - 1])
+        )
+    }
+
+    return result
 };
+
+
